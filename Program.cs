3e2bc0f5d6089fe4813d8af6e921e1d23b638e7e
Gerel <61454629+Gerlee01@users.noreply.github.com>
2020-02-26 12:00:02 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gereltsetseg
+{
+    namespace Lab1
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Employee emp = new Employee();
+                emp.subname = "Gerlee";
+                emp.subage = 20;
+                emp.subid = 1;
+                emp.subrank = "Senior";
+                Console.WriteLine("Hello world");
+                Console.WriteLine("Id : " + emp.subid + " Name : " + emp.subname + " Age : " + emp.subage + " Rank : " + emp.subrank);
+            }
+        }
+    }
+}
